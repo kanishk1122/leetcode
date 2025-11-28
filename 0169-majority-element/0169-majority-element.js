ff2fc5ee8dca2983447528fc5b86842b7ae32665
kanishk soni <102342556+kanishk1122@users.noreply.github.com>
2025-11-29 00:29:24 +0530

    Time: 2 ms (74.56%), Space: 58 MB (42.51%) - LeetHub

--- 0169-majority-element/0169-majority-element.js
+++ 0169-majority-element/0169-majority-element.js
@@ -2,7 +2,12 @@
  * @param {number[]} nums
  * @return {number}
  */
-var majorityElement = function(nums) {
-    nums.sort((a, b) => a - b); 
-    return nums[Math.floor(nums.length / 2)];
+var majorityElement = function (nums) {
+    let candidate = null;
+    let count = 0;
+    for (let num of nums) {
+        if (count === 0) candidate = num;
+        count += (num === candidate) ? 1 : -1;
+    }
+    return candidate;
 };
