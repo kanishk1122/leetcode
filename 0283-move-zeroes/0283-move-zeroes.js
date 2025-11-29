657519038758915ae0e4d4fe6ab4ae5eedbbd864
kanishk soni <102342556+kanishk1122@users.noreply.github.com>
2025-11-29 12:58:28 +0530

    Time: 0 ms (100%), Space: 60.1 MB (48.71%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function (nums) {
+    if (nums.indexOf(0) == -1) {
+        return;
+    }
+    let left = 0
+    for (let right = 0; right < nums.length; right++) {
+        if (nums[right] !== 0) {
+            if (left !== right) {
+                nums[left] ^= nums[right];
+                nums[right] ^= nums[left];
+                nums[left] ^= nums[right];
+            }
+            left++;
+        }
+    }
+};
