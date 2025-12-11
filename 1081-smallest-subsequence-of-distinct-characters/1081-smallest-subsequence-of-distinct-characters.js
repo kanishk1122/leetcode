aabae09964d0b6e028271e3380a71258443bc0dd
kanishk soni <102342556+kanishk1122@users.noreply.github.com>
2025-12-12 00:15:05 +0530

    Time: 2 ms (80.39%), Space: 57.5 MB (7.84%) - LeetHub

---
+++ 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var smallestSubsequence = function (s) {
+    const freq = new Map(), vis = new Set(), str = s.split(''), stack = [];
+    str.forEach(e => freq.set(e, (freq.get(e) + 1 || 1)))
+    str.forEach(e => {
+        if (!vis.has(e)) {
+            while (stack.length && stack[stack.length - 1] > e && freq.get(stack[stack.length - 1]))
+                vis.delete(stack.pop())
+
+            if (!vis.has(e))
+                stack.push(e), vis.add(e);
+        }
+        freq.set(e, freq.get(e) - 1)
+    })
+    return stack.join('')
+};
