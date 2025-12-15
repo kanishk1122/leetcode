0dcdd1a397f77c47d0a8f9a2f9b571c176c4a4bb
kanishk soni <102342556+kanishk1122@users.noreply.github.com>
2025-12-15 12:10:44 +0530

    Time: 3 ms (80%), Space: 65.1 MB (95%) - LeetHub

---
+++ 2110-number-of-smooth-descent-periods-of-a-stock/2110-number-of-smooth-descent-periods-of-a-stock.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var getDescentPeriods = function (prices) {
+    let count = 1
+    let right = 1
+    for (let i = 1; i < prices.length; i++) {
+        if(prices[i] == prices[i-1] - 1){
+            right++
+        }else{
+            right = 1
+        }
+        count += right
+    }
+    return count
+};
