2dcb20da8aa90bed3070c1d458ae08a5bd36e260
kanishk soni <102342556+kanishk1122@users.noreply.github.com>
2025-12-17 23:21:04 +0530

    Time: 7 ms (22.65%), Space: 59.2 MB (73.8%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.js
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.js
@@ -2,13 +2,11 @@
  * @param {number[][]} matrix
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
-var setZeroes = function(matrix) {
-   let cols = new Set()
-   let rows  = new Set()
-
+var setZeroes = function (matrix) {
+    let cols = new Set()
+    let rows = new Set()
     let m = matrix.length;
     let n = matrix[0].length;
-
     for (let i = 0; i < m; i++) {
         for (let j = 0; j < n; j++) {
             if (matrix[i][j] === 0) {
@@ -17,10 +15,9 @@ var setZeroes = function(matrix) {
             }
         }
     }
-
     for (let i = 0; i < m; i++) {
         for (let j = 0; j < n; j++) {
-            if(cols.has(j) || rows.has(i)){
+            if (cols.has(j) || rows.has(i)) {
                 matrix[i][j] = 0
             }
         }
