fc913322be0fa37caccedf3ffa25c323a9799546
kanishk soni <102342556+kanishk1122@users.noreply.github.com>
2025-12-20 17:59:28 +0530

    Time: 19 ms (12.93%), Space: 62.1 MB (13.36%) - LeetHub

---
+++ 0636-exclusive-time-of-functions/0636-exclusive-time-of-functions.js
@@ -0,0 +1,43 @@
+/**
+ * @param {number} n
+ * @param {string[]} logs
+ * @return {number[]}
+ */
+var exclusiveTime = function(n, logs) {
+    let stack = [];
+    let data = new Map();
+    let result = new Array(n).fill(0);
+    let func = 0;
+
+    for (let i = 0; i < logs.length; i++) {
+        let [idStr, command, timeStr] = logs[i].split(":");
+        let id = Number(idStr);
+        let timeline = Number(timeStr);
+
+        if (command === "start") {
+            // If something is already running, update its start time forward
+            if (stack.length > 0) {
+                let top = stack[stack.length - 1]; 
+                result[top[1]] += timeline - data.get(top[0]);
+            }
+            
+            stack.push([func, id]);
+            data.set(func, timeline);
+            func++;
+
+        } else { // end
+            let [funcIndex, funcId] = stack.pop();
+            let startTime = data.get(funcIndex);
+
+            result[funcId] += timeline - startTime + 1;
+
+            // Update the start time of the previous function
+            if (stack.length > 0) {
+                let top = stack[stack.length - 1];
+                data.set(top[0], timeline + 1);
+            }
+        }
+    }
+
+    return result;
+};
