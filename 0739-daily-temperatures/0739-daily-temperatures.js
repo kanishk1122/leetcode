2e8778772cab20295435b539cde316fb37b16eb6
kanishk soni <102342556+kanishk1122@users.noreply.github.com>
2025-12-21 00:43:11 +0530

    Time: 22 ms (45.19%), Space: 76.4 MB (60.37%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} temperatures
+ * @return {number[]}
+ */
+var dailyTemperatures = function(temperatures) {
+    let result = new Array(temperatures.length).fill(0)
+    let stack = []
+    for (let i = 0; i < temperatures.length; i++) {
+        while (stack.length > 0 && temperatures[i] > temperatures[stack[stack.length - 1]]) {
+            let top = stack.pop();
+            result[top] = i - top;
+        }
+        stack.push(i)
+    }
+    console.log(result)
+    return result
+};
