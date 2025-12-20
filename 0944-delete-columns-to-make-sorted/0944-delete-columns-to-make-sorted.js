3f45bfc485966e634a346411c3d5961bb5e0d0c6
kanishk soni <102342556+kanishk1122@users.noreply.github.com>
2025-12-20 17:39:29 +0530

    Time: 9 ms (39.08%), Space: 58.4 MB (70.11%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.js
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.js
@@ -6,15 +6,15 @@ var minDeletionSize = function (strs) {
     let count = 0
     let n = strs.length
     let m = strs[0].length
-    let col = new Set()
+    let col = {}
     for (let i = 1; i < n; i++) {
         let j = 0
         for (j; j < m; j++) {
-            if (col.has(j)) {
+            if (col[j]) {
                 continue
             }
             if (strs[i][j] < strs[i - 1][j]) {
-                col.add(j)
+                col[j] = true
                 count++
             }
         }
