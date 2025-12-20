21fabc82190f3ca6c661fa0653294e64ff5a0bc3
kanishk soni <102342556+kanishk1122@users.noreply.github.com>
2025-12-20 17:37:17 +0530

    Time: 9 ms (39.08%), Space: 57.2 MB (97.7%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.js
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.js
@@ -2,18 +2,18 @@
  * @param {string[]} strs
  * @return {number}
  */
-var minDeletionSize = function(strs) {
-    let count = 0 
+var minDeletionSize = function (strs) {
+    let count = 0
     let n = strs.length
     let m = strs[0].length
     let col = {}
-    for(let i = 1;i < n;i++){
+    for (let i = 1; i < n; i++) {
         let j = 0
-        for(j;j<m;j++){
-            if(col[j]){
+        for (j; j < m; j++) {
+            if (col[j]) {
                 continue
             }
-            if(strs[i][j] < strs[i-1][j]){
+            if (strs[i][j] < strs[i - 1][j]) {
                 col[j] = true
                 count++
             }
