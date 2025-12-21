ef5432c86e79a4886388d7da75470e62fd1c7caf
kanishk soni <102342556+kanishk1122@users.noreply.github.com>
2025-12-21 22:31:00 +0530

    Time: 21 ms (48.41%), Space: 68.5 MB (66.79%) - LeetHub

---
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} heights
+ * @return {number}
+ */
+var largestRectangleArea = function (heights) {
+    let stack = []
+    let max = 0
+     for (let i = 0; i <= heights.length; i++) {
+        let curHeight = i === heights.length ? 0 : heights[i]
+        while (stack.length && heights[stack[stack.length - 1]] > curHeight) {
+            let top = stack.pop()
+            let height = heights[top]
+            let width = stack.length === 0 ? i : i - stack[stack.length - 1] - 1
+            max = Math.max(max, height * width)
+        }
+        stack.push(i)
+    }
+    console.log(max)
+    return max
+};
