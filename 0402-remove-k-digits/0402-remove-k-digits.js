abdce3b1662ea455ef2bbb4b7afbc81708f12d27
kanishk soni <102342556+kanishk1122@users.noreply.github.com>
2025-12-22 17:07:05 +0530

    Time: 12 ms (46.89%), Space: 60 MB (68.88%) - LeetHub

---
+++ 0402-remove-k-digits/0402-remove-k-digits.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} num
+ * @param {number} k
+ * @return {string}
+ */
+var removeKdigits = function(num, k) {
+    const stack = [];
+    for (let digit of num) {
+        while (k > 0 && stack.length && stack[stack.length - 1] > digit) {
+            stack.pop();
+            k--;
+        }
+        stack.push(digit);
+    }
+
+    stack.splice(stack.length - k, k)
+    const result = stack.join('').replace(/^0+/, '');
+    return result === '' ? '0' : result;
+};
