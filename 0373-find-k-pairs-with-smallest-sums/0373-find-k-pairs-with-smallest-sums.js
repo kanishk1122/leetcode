88566f43c90f36c832183f4447034523cf0479bb
kanishk soni <102342556+kanishk1122@users.noreply.github.com>
2026-01-01 22:46:30 +0530

    Time: 48 ms (85.66%), Space: 85.7 MB (66.18%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.js
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.js
@@ -6,27 +6,28 @@
  */
 var kSmallestPairs = function (nums1, nums2, k) {
     if (!nums1.length || !nums2.length || k === 0) return [];
-    let minheap = new MinPriorityQueue(x => x[0])
+    let minheap = new MinPriorityQueue(x => x.sum);
     let result = []
     let n = Math.min(k, nums1.length)
     for (let i = 0; i < n; i++) {
-        minheap.enqueue([
-            nums1[i] + nums2[0],
+        minheap.push({
+            sum: nums1[i] + nums2[0],
             i,
-            0
-        ]);
+            j: 0
+        });
     }
+
     while (k > 0 && !minheap.isEmpty()) {
-        const [sum, i, j] = minheap.dequeue();
+        const { i, j } = minheap.pop();
         result.push([nums1[i], nums2[j]]);
         k--;
 
         if (j + 1 < nums2.length) {
-            minheap.enqueue([
-                nums1[i] + nums2[j + 1],
+            minheap.push({
+                sum: nums1[i] + nums2[j + 1],
                 i,
-                j + 1
-            ]);
+                j: j + 1
+            });
         }
     }
 
