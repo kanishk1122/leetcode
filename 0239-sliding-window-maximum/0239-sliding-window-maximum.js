a8bbd54c127df5583eb17755f6b036b8c22a9747
kanishk soni <102342556+kanishk1122@users.noreply.github.com>
2026-01-03 00:10:08 +0530

    Time: 30 ms (95.61%), Space: 86 MB (75.61%) - LeetHub

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var maxSlidingWindow = function(nums, k) {
+    let queue = []
+    let result = []
+    for(let i =0;i<nums.length;i++){
+       if(queue.length && queue[0] <= i -k){
+        queue.shift()
+       }
+       while(queue.length && nums[queue[queue.length -1]] <= nums[i] ){
+            queue.pop()
+       }
+       queue.push(i);
+       if(i >= k - 1){
+        result.push(nums[queue[0]])
+       }
+    }
+    return result
+};
