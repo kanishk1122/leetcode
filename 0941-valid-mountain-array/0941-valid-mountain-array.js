0a449d354097670bb3507fcb8d038c3211da7587
kanishk soni <102342556+kanishk1122@users.noreply.github.com>
2026-01-07 22:07:52 +0530

    Time: 62 ms (12%), Space: 56.8 MB (84.44%) - LeetHub

--- 0941-valid-mountain-array/0941-valid-mountain-array.js
+++ 0941-valid-mountain-array/0941-valid-mountain-array.js
@@ -7,36 +7,32 @@ var validMountainArray = function (arr) {
         return false
     }
     let stack = []
-    stack.push(arr[0])
     let inclining = true
     let tophill = 0
+    let lasthill = arr[0]
     for (let i = 1; i < arr.length; i++) {
-        console.log(stack)
-        if(arr[tophill] < arr[i]){
-            tophill = i
-        }
         if (inclining) {
-            if (stack[stack.length - 1] < arr[i]) {
-                stack.pop()
-                stack.push(arr[i])
+            if (arr[tophill] < arr[i]) {
+                tophill = i
+            }
+            if (lasthill < arr[i]) {
+                lasthill = arr[i]
             } else if (
-                stack[stack.length - 1] > arr[i]
+                lasthill > arr[i]
             ) {
-                stack.pop()
-                stack.push(arr[i])
+                lasthill = arr[i]
                 inclining = false
             } else {
                 return false
             }
         } else {
-            if (stack[stack.length - 1] > arr[i]) {
-                stack.pop()
-                stack.push(arr[i])
+            if (lasthill > arr[i]) {
+                lasthill = arr[i]
             } else {
                 return false
             }
         }
     }
     console.log(tophill)
-    return inclining ? false :tophill == 0 ? false : true
+    return inclining ? false : tophill == 0 ? false : true
 };
