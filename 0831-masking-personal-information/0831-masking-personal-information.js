cf4962e90744f2c9fd183eefa31c3408aa95545e
kanishk soni <102342556+kanishk1122@users.noreply.github.com>
2026-01-10 12:02:45 +0530

    Time: 0 ms (100%), Space: 52.6 MB (97.7%) - LeetHub

--- 0831-masking-personal-information/0831-masking-personal-information.js
+++ 0831-masking-personal-information/0831-masking-personal-information.js
@@ -13,17 +13,12 @@ var maskPII = function(s) {
         return mail + domain
     }else{
         s = s.replace(/[()\-\+\s]/g, "")
-        console.log({s})
         let mask = ""
         n = s.length
-        console.log(n)
         let lastlocalnumber = s.slice(-4, n)
         let frontnums = s.slice(0, -4)
-        // console.log({frontnums})
-        // console.log(s)
         if(frontnums.length == 6){
             return "***-***-" +lastlocalnumber 
-            // console.log(s.slice(-4),{lastlocalnumber})
         }else{
             let m = frontnums.length - 6 
             let extramap = "+"
@@ -31,7 +26,6 @@ var maskPII = function(s) {
                 extramap += "*"
                 m--
             }
-
             return extramap + "-***-***-" + lastlocalnumber
         }
     }
