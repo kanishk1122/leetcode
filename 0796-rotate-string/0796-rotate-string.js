7e995f8adfa4a147ec69081d0f1657a5cdb0f99a
kanishk soni <102342556+kanishk1122@users.noreply.github.com>
2026-01-14 21:44:45 +0530

    Time: 3 ms (11.33%), Space: 55.8 MB (9.38%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.js
+++ 0796-rotate-string/0796-rotate-string.js
@@ -4,7 +4,9 @@
  * @return {boolean}
  */
 var rotateString = function(s, goal) {
+    if(s.length !== goal.length) return false
     sarr = s.split("")
+    
     for(let chat of s){
         let temp = sarr.shift()
         sarr.push(temp)
